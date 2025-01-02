bc0f65644d50302b9455aeee8f39bfa03bc0f6db
add0794 <add0794@gmail.com>
2025-01-02 16:15:31 -0500

    incorporate gradient descent into OOP

--- oop_version.py
+++ oop_version.py
@@ -1,11 +1,6 @@
 import pandas as pd
 import memory_profiler
-from oop_methods import DataLoader, CustomLinearRegression, Visualizer, MemoryComparisonRegression
-
-# python -m memory_profiler oop_methods.py -o my_memory_profile.txt
-
-# python -m memory_profiler oop_version.py 
-
+from oop_methods import DataLoader, CustomLinearRegression, Visualizer
 
 # Initialize loader object, with the target (label) column and categorical variable(s) to be dummy coded
 loader = DataLoader(target_column="Performance Index", categorical_columns=["Extracurricular Activities"])
@@ -48,19 +43,21 @@ for case in cases:
     results_df = pd.DataFrame()
     coefficients_df = pd.DataFrame()
     
-    for method in ['numpy', 'scipy', 'statsmodels', 'scikit-learn', 'ridge', 'lasso']:
+    for method in ['numpy', 'scipy', 'statsmodels', 'scikit-learn', 'ridge', 'lasso', 'gradient descent']:
         if method == 'numpy':
-            elapsed_time, memory_usage, coefficients = lr_model.fit_numpy(case)
+            elapsed_time, coefficients = lr_model.fit_numpy(case)
         elif method == 'scipy':
-            elapsed_time, memory_usage, coefficients = lr_model.fit_scipy(case)
+            elapsed_time, coefficients = lr_model.fit_scipy(case)
         elif method == 'statsmodels':
-            elapsed_time, memory_usage, coefficients = lr_model.fit_statsmodels(case)
+            elapsed_time, coefficients = lr_model.fit_statsmodels(case)
         elif method == 'scikit-learn':
-            elapsed_time, memory_usage, coefficients = lr_model.fit_sklearn(case)
+            elapsed_time, coefficients = lr_model.fit_sklearn(case)
         elif method == 'ridge':
-            elapsed_time, memory_usage, coefficients = lr_model.fit_ridge(case, ridge_alpha=ridge_alpha)            
+            elapsed_time, coefficients = lr_model.fit_ridge(case, ridge_alpha=ridge_alpha)            
         elif method == 'lasso':
-            elapsed_time, memory_usage, coefficients = lr_model.fit_lasso(case, lasso_alpha=lasso_alpha)
+            elapsed_time, coefficients = lr_model.fit_lasso(case, lasso_alpha=lasso_alpha)
+        elif method == 'gradient descent':
+            elapsed_time, coefficients = lr_model.fit_gradient_descent(case, learning_rate=0.000001, epochs=100, precision=0.00001)
         else:
             raise Exception("Please enter a correct method.")
 
@@ -68,7 +65,7 @@ for case in cases:
             [
                 results_df,
                 pd.DataFrame(
-                    {'Method': [method], 'Elapsed Time': [elapsed_time], 'Memory Usage': [memory_usage]}
+                    {'Method': [method], 'Elapsed Time': [elapsed_time]}
                 )
             ],
             axis=0,
@@ -92,75 +89,3 @@ for case in cases:
     print(f"\nResults for Extracurricular Activities: {case}")
     print(results[case]['results_df'])
     print(results[case]['coefficients_df'])
-
-
-# lr_comparison = MemoryComparisonRegression(features_yes=yes_X, features_no=no_X, label=label, cols_yes=yes_cols, cols_no=no_cols)
-
-# # Compare methods
-# results_df = lr_comparison.compare_methods(
-#     case='yes',
-#     methods=['numpy', 'scipy', 'statsmodels', 'scikit-learn', 'ridge', 'lasso'],
-#     ridge_alpha=100.0,
-#     lasso_alpha=100.0
-# )
-
-# print(results_df)
-
-
-# lr_model = CustomLinearRegression(features_yes=yes_X, features_no=no_X, label=label, cols_yes=yes_cols, cols_no=no_cols)
-
-# cases = ['no', 'yes']
-# results = {}  # Store results for both cases
-
-# # Define Ridge and Lasso alpha values
-# ridge_alpha = 100.0
-# lasso_alpha = 100.0
-
-# for case in cases:
-#     results_df = pd.DataFrame()
-#     coefficients_df = pd.DataFrame()
-    
-#     for method in ['numpy', 'scipy', 'statsmodels', 'scikit-learn', 'ridge', 'lasso']:
-#         if method == 'numpy':
-#             elapsed_time, memory_usage, coefficients = lr_model.fit_numpy(case)
-#         elif method == 'scipy':
-#             elapsed_time, memory_usage, coefficients = lr_model.fit_scipy(case)
-#         elif method == 'statsmodels':
-#             elapsed_time, memory_usage, coefficients = lr_model.fit_statsmodels(case)
-#         elif method == 'scikit-learn':
-#             elapsed_time, memory_usage, coefficients = lr_model.fit_sklearn(case)
-#         elif method == 'ridge':
-#             elapsed_time, memory_usage, coefficients = lr_model.fit_ridge(case, ridge_alpha=ridge_alpha)            
-#         elif method == 'lasso':
-#             elapsed_time, memory_usage, coefficients = lr_model.fit_lasso(case, lasso_alpha=lasso_alpha)
-#         else:
-#             raise Exception("Please enter a correct method.")
-
-#         results_df = pd.concat(
-#             [
-#                 results_df,
-#                 pd.DataFrame(
-#                     {'Method': [method], 'Elapsed Time': [elapsed_time], 'Memory Usage': [memory_usage]}
-#                 )
-#             ],
-#             axis=0,
-#             ignore_index=True
-#         )
-#         coefficients_df = pd.concat(
-#             [
-#                 coefficients_df,
-#                 pd.DataFrame(
-#                     {f'{method}_{case}': coefficients},
-#                     index=(yes_cols if case == 'yes' else no_cols)
-#                 )
-#             ],
-#             axis=1
-#         )
-
-#     results[case] = {'results_df': results_df, 'coefficients_df': coefficients_df}
-
-# # Print results neatly after the loop
-# for case in cases:
-#     print(f"\nResults for Extracurricular Activities: {case}")
-#     print(results[case]['results_df'])
-#     print(results[case]['coefficients_df'])
